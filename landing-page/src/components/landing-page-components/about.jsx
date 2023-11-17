832cda338335c03900e8240e6165b39e1780b485
Isuru Sasanga Kolombage <isuru.k@elysiancrest.com>
2023-11-17 15:44:41 +0530

    create about and footer

--- landing-page/src/components/landing-page-components/about.jsx
+++ landing-page/src/components/landing-page-components/about.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import React from 'react' 
+import proPic from './Images/proPic.png'
 
 function About() {
   return (
-    <div>
-      <h1 className="text-red-600">About the hosts</h1>
+    <div className='text-center bg-gray-200'>
+      <h1 className="text-xl font font-medium py-10">About the hosts</h1>
+
+      <div className='flex justify-center align-center gap-10'>
+        <div className='w-1/5 flex flex-col items-center'>
+            <h2>Anne Keger</h2>
+            <h3 className='text-gray-500'>Assistent manager</h3>
+            <p className='text-gray-500 pt-3 text-sm'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Perferendis molestiae eligendi quod! Assumenda inventore corporis molestiae perferendis distinctio blanditiis iusto nulla cupiditate voluptatem fugit est quaerat dolorum natus, necessitatibus accusantium.</p>
+            <img src= {proPic} alt='proPic' className='h-60' />
+        </div>
+        <div className='w-1/5 flex flex-col items-center'>
+            <h2>Jessica Yang</h2>
+            <h3 className='text-gray-500'>Assistent manager</h3>
+            <p className='text-gray-500 pt-3 text-sm'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Perferendis molestiae eligendi quod! Assumenda inventore corporis molestiae perferendis distinctio blanditiis iusto nulla cupiditate voluptatem fugit est quaerat dolorum natus, necessitatibus accusantium.</p>
+            <img src= {proPic} alt='proPic' className='h-60' />
+        </div>
+        <div className='w-1/5 flex flex-col items-center'>
+            <h2>Weroonika Weerasinghe</h2>
+            <h3 className='text-gray-500'>Assistent manager</h3>
+            <p className='text-gray-500 pt-3 text-sm'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Perferendis molestiae eligendi quod! Assumenda inventore corporis molestiae perferendis distinctio blanditiis iusto nulla cupiditate voluptatem fugit est quaerat dolorum natus, necessitatibus accusantium.</p>
+            <img src= {proPic} alt='proPic' className='h-60' />
+        </div>
+      </div>
     </div>
   )
 }
