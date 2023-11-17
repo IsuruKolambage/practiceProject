5d3f05ca071a3101b11398f077721befd8f3d763
RashmikaE <rashmika.e@elysiancrest.com>
2023-11-17 14:57:02 +0530

    header image added

--- landing-page/src/components/landing-page-components/header.jsx
+++ landing-page/src/components/landing-page-components/header.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import headerImg from './Images/headerImage.jpg'
 
 function Header() {
   return (
-    <div className='mt-10 ml-5'>
-        <div className='mt-10 ml-5'> This is Header</div>
+    <div className=''>
+        
+        <img src= {headerImg} alt='HeaderImage' className='w-full h-1/2 z-1 opacity-80' />
+        
+        <div className="absolute top-0 left-0 text-white p-4">
+        <h1 className="text-4xl font-bold">Starting your SEO Adventure with Positionly</h1>
+      </div>
     </div>
   )
 }
