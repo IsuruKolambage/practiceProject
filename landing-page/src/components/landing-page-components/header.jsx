f57337028a92749151fadfd60de9c374825a36ab
RashmikaE <rashmika.e@elysiancrest.com>
2023-11-17 15:44:38 +0530

    changes finalized

--- landing-page/src/components/landing-page-components/header.jsx
+++ landing-page/src/components/landing-page-components/header.jsx
@@ -5,10 +5,14 @@ function Header() {
   return (
     <div className=''>
         
-        <img src= {headerImg} alt='HeaderImage' className='w-full h-1/2 z-1 opacity-80' />
+        <img src= {headerImg} alt='HeaderImage' className='w-full h-1/2 z-1 ' />
         
-        <div className="absolute top-0 left-0 text-white p-4">
-        <h1 className="text-4xl font-bold">Starting your SEO Adventure with Positionly</h1>
+        <div className="absolute top-1/4 left-5 text-white p-4">
+        <h1 className="text-7xl font-bold mb-5 ">Starting your SEO Adventure <br/> with Positionly</h1>
+        <p className='2xl mb-10'> Lorem Ipsum has been the industry's standard dummy text ever since the </p>
+        <h2 className='text-3xl my-10 font-bold'> TUESDAY, JUNE 9th, 2015, <br/> DURATION 120 MINUTES </h2>
+
+        <button className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded "> Register Now </button>
       </div>
     </div>
   )
